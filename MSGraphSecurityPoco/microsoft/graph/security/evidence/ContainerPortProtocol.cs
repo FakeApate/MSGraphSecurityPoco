50ae91d0ece70cf9c11549035cd9d4ac5a3ad606
FakeApate <imboden.samuel@protonmail.ch>
2025-12-07 16:10:31 +0100

    Remove Evidence Types that are "useless"

--- MSGraphSecurityPoco/microsoft/graph/security/evidence/ContainerPortProtocol.cs
+++
@@ -1,9 +0,0 @@
-namespace MSGraphSecurityPoco.Microsoft.Graph.Security.Evidence;
-
-public enum ContainerPortProtocol
-{
-    udp = 0,
-    tcp = 1,
-    sctp = 2,
-    unknownFutureValue = 3
-}
--- MSGraphSecurityPoco/microsoft/graph/security/evidence/ContainerRegistryEvidence.cs
+++
@@ -1,9 +0,0 @@
-namespace MSGraphSecurityPoco.Microsoft.Graph.Security.Evidence;
-
-public partial record ContainerRegistryEvidence : AlertEvidence
-{
-    public string? Registry { get; init; }
-
-
-
-}
